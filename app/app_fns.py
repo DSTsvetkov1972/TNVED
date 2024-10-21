aea20bcc54b0473210d40c4d46db828ed2d2b366
Цветков Дмитрий Сергеевич <tsvetkovds@trcont.ru>
2024-10-21 09:32:11 +0300

    read csv one time

--- app/app_fns.py
+++ app/app_fns.py
@@ -9,18 +9,29 @@ def data_parcer(data_str):
     except:
         return None
 
-def tnved_1():
+
+def change_button_status(session_state, key):
+    if session_state[key]:
+        session_state[key] = False
+    else:
+        session_state[key] = True
+
+
+
+
+def get_tnved_1_df():
     df = pd.read_csv('TNVED1.TXT',
-                     encoding = 'cp866',
-                     sep='|', usecols=[0, 1, 2, 3],
-                     names=['id1', 'name1', 'rem1', 'date'],
-                     skiprows=1,
-                     dtype='str')
+                    encoding = 'cp866',
+                    sep='|', usecols=[0, 1, 2, 3],
+                    names=['id1', 'name1', 'rem1', 'date'],
+                    skiprows=1,
+                    dtype='str')
     df = df.fillna("")    
-
     df['date'] = df['date'].apply(data_parcer)
-    ids1 = sorted(list(set(df['id1'])))
+    return df
 
+def tnved_1(df=get_tnved_1_df()):
+    ids1 = sorted(list(set(df['id1'])))
     res = {}
     for id1 in ids1:
         id1_df = df[(df['id1']==id1)]
@@ -28,18 +39,21 @@ def tnved_1():
         res[id1] = dict(id1_df[['name1','rem1']].iloc[0])
     return res
 
-def tnved_2(id1='01'):
+
+def get_tnved_2_df():
     df = pd.read_csv('TNVED2.TXT',
-                     encoding = 'cp866',
-                     sep='|',
-                     usecols=[0, 1, 2, 3, 4],
-                     names=['id1', 'id2', 'name2', 'rem2', 'date'],
-                     skiprows=1,
-                     dtype='str')
+                    encoding = 'cp866',
+                    sep='|',
+                    usecols=[0, 1, 2, 3, 4],
+                    names=['id1', 'id2', 'name2', 'rem2', 'date'],
+                    skiprows=1,
+                    dtype='str')
     df = df.fillna("")
     df['date'] = df['date'].apply(data_parcer)
-    id1_df = df[df['id1']==id1]
+    return df
 
+def tnved_2(df=get_tnved_2_df(), id1='01'):
+    id1_df = df[df['id1']==id1]
     ids2 = sorted(list(set(id1_df['id2'])))
 
     res = {}
@@ -49,21 +63,22 @@ def tnved_2(id1='01'):
         res[id2] = dict(id2_df[['name2', 'rem2']].iloc[0])
     return res
 
-def tnved_3(id2='01'):
+def get_tnved_3_df():
     df = pd.read_csv('TNVED3.TXT',
-                     encoding = 'cp866',
-                     sep='|',
-                     usecols=[0, 1, 2, 3],
-                     names=['id2', 'id3', 'name3', 'date'],
-                     skiprows=1,
-                     dtype='str')
+                    encoding = 'cp866',
+                    sep='|',
+                    usecols=[0, 1, 2, 3],
+                    names=['id2', 'id3', 'name3', 'date'],
+                    skiprows=1,
+                    dtype='str')
     df = df.fillna("")
     df['date'] = df['date'].apply(data_parcer)
+    return df
 
+
+def tnved_3(df=get_tnved_3_df(), id2='01'):
     id2_df = df[df['id2']==id2]
-    
     ids3 = sorted(list(set(id2_df['id3'])))
-
     res = {}
     for id3 in ids3:
         id3_df = id2_df[id2_df['id3']==id3]
@@ -71,16 +86,21 @@ def tnved_3(id2='01'):
         res[id3] = dict(id3_df[['name3']].iloc[0])
     return res
 
-def tnved_4(id2='01', id3='01'):
+
+def get_tnved_4_df():
     df = pd.read_csv('TNVED4.TXT',
-                     encoding = 'cp866',
-                     sep='|',
-                     usecols=[0, 1, 2, 3, 4],
-                     names=['id2', 'id3', 'id4', 'name4', 'date'],
-                     skiprows=1,
-                     dtype='str') 
+                    encoding = 'cp866',
+                    sep='|',
+                    usecols=[0, 1, 2, 3, 4],
+                    names=['id2', 'id3', 'id4', 'name4', 'date'],
+                    skiprows=1,
+                    dtype='str') 
     df = df.fillna("")    
     df['date'] = df['date'].apply(data_parcer)
+    return df
+
+def tnved_4(df=get_tnved_4_df(), id2='01', id3='01'):
+
     id23_df = df[(df['id2']==id2) & (df['id3']==id3)]
     ids4 = sorted(list(set(id23_df['id4'])))
 
@@ -92,17 +112,7 @@ def tnved_4(id2='01', id3='01'):
         
     return res
 
-def change_label_style(label, font_size='12px', font_color='black', font_family='sans-serif'):
-    html = f"""
-    <script>
-        var elems = window.parent.document.querySelectorAll('p');
-        var elem = Array.from(elems).find(x => x.innerText == '{label}');
-        elem.style.fontSize = '{font_size}';
-        elem.style.color = '{font_color}';
-        elem.style.fontFamily = '{font_family}';
-    </script>
-    """
-    st.components.v1.html(html)
+
 
 if __name__ == '__main__':
     print(Fore.YELLOW, tnved_1(), Fore.WHITE)
--- app/ui.py
+++ app/ui.py
@@ -1,23 +1,27 @@
 import streamlit as st
-from app_fns import tnved_1, tnved_2, tnved_3, tnved_4
+from app_fns import (get_tnved_1_df, get_tnved_2_df, get_tnved_3_df, get_tnved_4_df,
+                     tnved_1, tnved_2, tnved_3, tnved_4)
+from colorama import Fore
 
-def change_label_style(label, font_size='12px', font_color='dark-grey', font_family='sans-serif'):
-    html = f"""
-    <script>
-        var elems = window.parent.document.querySelectorAll('p');
-        var elem = Array.from(elems).find(x => x.innerText == '{label}');
-        elem.style.fontSize = '{font_size}';
-        elem.style.color = '{font_color}';
-        elem.style.fontFamily = '{font_family}';
-    </script>
-    """
-    st.components.v1.html(html)
+if 'tnved_1_df' not in st.session_state:
+    st.session_state.tnved_1_df = get_tnved_1_df()
+if 'tnved_2_df' not in st.session_state:
+    st.session_state.tnved_2_df = get_tnved_2_df()
+if 'tnved_3_df' not in st.session_state:
+    st.session_state.tnved_3_df = get_tnved_3_df()
+if 'tnved_4_df' not in st.session_state:
+    st.session_state.tnved_4_df = get_tnved_4_df()
+
+# print(Fore.RED, st.session_state.tnved_1_df)
+# print(Fore.YELLOW, st.session_state.tnved_2_df)
+# print(Fore.GREEN, st.session_state.tnved_3_df)
+# print(Fore.BLUE, st.session_state.tnved_4_df, Fore.WHITE)
 
 st.markdown('<h1 style="color:darkred; font-weight:bold; font-size:48px; text-align:center">Товарная номенклатура внешнеэкономической деятельности (ТНВЭД)</h1>', unsafe_allow_html=True)  
 st.markdown('<p style="color:darkred; text-align:center">'
             'По данным сайта <a href="(https://www.nalog.gov.ru/rn77/program/5961290/">www.nalog.gov.ru</a> опубликованным 17-10-2024', unsafe_allow_html=True)
 
-ids1 = tnved_1()
+ids1 = tnved_1(st.session_state.tnved_1_df)
 for id1, d1 in ids1.items():
     if f'{id1}_state' not in st.session_state:
         st.session_state[f'{id1}_state'] = False
@@ -42,9 +46,9 @@ for id1, d1 in ids1.items():
                     st.session_state[f'{id1}_rem_state'] = True
             if st.session_state[f'{id1}_rem_state']:        
                 st.markdown(f'<p style="color:firebrick;">{d1["rem1"]}</p>', unsafe_allow_html=True)
-        ids2 = tnved_2(id1)
+        ids2 = tnved_2(st.session_state.tnved_2_df, id1)
         for id2, d2 in ids2.items():
-            ids3 = tnved_3(id2)
+            ids3 = tnved_3(st.session_state.tnved_3_df, id2)
 
             with st.expander(f"**{id1} {id2}. {d2['name2']}**"):
                 if d2['rem2']:
@@ -72,7 +76,7 @@ for id1, d1 in ids1.items():
                             st.session_state[f'{id1}_{id2}_{id3}_state'] = True
 
                     if st.session_state[f'{id1}_{id2}_{id3}_state']:    
-                        ids4 = tnved_4(id2, id3)
+                        ids4 = tnved_4(st.session_state.tnved_4_df, id2, id3)
                         for id4, d4 in ids4.items():
                             string = f" - _{id1} {id2} {id3} {id4}. {d4['name4']}_"
                             #change_label_style(string, font_size='8px', font_color='red', font_family='sans-serif')
